2d1fb9f0c75658baeb9aca465fa543305d8e6dac
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-08-23 12:46:39 +0530

    added the root page blogslist

--- src/Components/BlogList.jsx
+++ src/Components/BlogList.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import blogs from '../blogs.json'
 
 const BlogList = () => {
+
+  const intialBlog = blogs[0]
+
   return (
-    <div>
-      <h1>this is blogging page</h1>
+    <div className='mt-5'>
+      <h1 className='text-5xl font-bold'>{intialBlog.name}</h1>
+      <h2 className='text-3xl font-bold mt-2'>{intialBlog.title}</h2>
+      <p className='mt-2'>{intialBlog.content}</p>
     </div>
   )
 }
