c5a10b9a9acfaa9c6c614c20c452c7f79bd2cc26
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-08-27 17:18:44 +0530

    thats not an problem we have to use redux or zustand now to apply the value

--- src/Components/Sample/InnerBody.jsx
+++ src/Components/Sample/InnerBody.jsx
@@ -3,11 +3,17 @@ import RichEdit from './RichEdit'
 import blogs from '../../blogs.json'
 
 const InnerBody = () => {
+
+  const checkBlog = blogs[1]
+
   return (
     <div className='inner-body'>
+    
       <RichEdit />
-      <div className='details-content mt-5'>content</div>
-      <div className='details-content mt-3'>content</div>
+      <div className='flex flex-col'>
+        <input className=' bg-transparent details-content mt-5 text-white font-semibold text-4xl' value={checkBlog.title}></input>
+        <textarea className='content-detail bg-transparent mt-3 text-white text-2xl'  value={checkBlog.content}></textarea>
+      </div>
     </div>
   )
 }
