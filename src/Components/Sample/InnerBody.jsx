e14aa51d35d6a7d954830cf2dc1895a2ae0f30f8
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-08-27 18:26:11 +0530

    added dropdown menu and providing the function to it

--- src/Components/Sample/InnerBody.jsx
+++ src/Components/Sample/InnerBody.jsx
@@ -1,26 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import RichEdit from './RichEdit'
 import blogs from '../../blogs.json'
+import DropdownMenu from '../DropdownMenu'
 
 const InnerBody = () => {
 
+  const [blogsData, setBlogsData] = useState(blogs)
+  const [selectedName, setSelectedName] =useState(blogs[0].name)
+  const [name , setName] = useState("")
+  const [title, setTitle] = useState("")
+  const [content , setContent] = useState("")
+
+  const selectedBlog = blogsData.find(blog => blog.name === selectedName)
 
   const checkBlog = blogs[1]
 
+  useEffect(() =>
+  {
+     
+    if (selectedBlog) {
+      setName(selectedBlog.name)
+      setTitle(selectedBlog.title)
+      setContent(selectedBlog.content)
+    }
+    
+  },[selectedName,blogsData])
+
   return (
     <div className='inner-body'>
+
+      <DropdownMenu
+        blogs={blogsData}
+        selectedName={selectedName}
+        onSelect={setSelectedName}
+      />
     
       <RichEdit />
       <div className='flex flex-col'>
 
-        <input className=' bg-transparent details-content mt-5 text-white font-semibold text-4xl'
-         value={checkBlog.title}
-         readOnly
+        <input 
+         className=' bg-transparent details-content mt-5 text-white font-semibold text-4xl'
+         value={title}
          ></input>
 
 
-        <textarea className='content-detail bg-transparent mt-3 text-white text-2xl' 
-         value={checkBlog.content}
+        <textarea 
+         className='content-detail bg-transparent mt-3 text-white text-2xl' 
+         value={content}
          readOnly
          ></textarea>
 
