b12f330545126466507380e0dc0749fcf4958cbe
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-08-27 15:43:28 +0530

    importing the blogs.json file by ../../blogs.json

--- src/Components/Sample/InnerBody.jsx
+++ src/Components/Sample/InnerBody.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import RichEdit from './RichEdit'
+import blogs from '../../blogs.json'
 
 const InnerBody = () => {
   return (
     <div className='inner-body'>
       <RichEdit />
+      <div className='details-content mt-5'>content</div>
+      <div className='details-content mt-3'>content</div>
     </div>
   )
 }
