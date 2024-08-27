f99099787d0a7bd855663597dbf0a1e382ee5c04
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-08-27 17:18:03 +0530

    added the user selection dropDown in the sidebar but the problem is the content is in the innerboady

--- src/Components/Sample/SideNav.jsx
+++ src/Components/Sample/SideNav.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DropdownMenu from '../DropdownMenu'
+import blogs from '../../blogs.json'
 
 const SideNav = () => {
+
+  const [blogsData, setBlogs] = useState(blogs)
+  const [selectedName, setSelectedName] = useState(blogs[0].name)
+  const [name, setName] = useState("")
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+
+  const selectedBlog = blogsData.find(blog => blog.name === selectedName)
+
+  useEffect(() => {
+    if (selectedBlog) {
+      setName(selectedBlog.name)
+      setTitle(selectedBlog.title)
+      setContent(selectedBlog.content)
+    }
+
+
+  }, [selectedName, blogsData])
+
+
+
   return (
     <div className='side-nav'>
-          <div className='user-selection text-white text-center'><p>User Selection</p></div>
+      <DropdownMenu
+        blogs={blogsData}
+        selectedName={selectedName}
+        onSelect={setSelectedName}
+      />
           <div className='side-bo'>
         <button><img src='../Header.png'></img>Header</button>
         <button><img src='../Header.png'></img>Title</button>
